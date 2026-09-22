1f4094ec3005fc67d4824c88fb21654f108846f7
Himanshu Patil <73234020+himanshupatil208@users.noreply.github.com>
2026-09-22 16:19:45 -0400

    Time: 627 ms (5.21%) | Memory: 72 MB (32.78%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+         for (int i = 0; i < n; i++) {
+            while (nums[i] <= n && nums[i] > 0 && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for(int i:nums)
+            System.out.print(n + " ");
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==i+1)
+                continue;
+            return i+1;
+        }
+
+        return n+1;
+    }
+}
